16088f63948df189a662095cfea6d54d8f77e12d
jnorth314 <56890989+jnorth314@users.noreply.github.com>
2020-10-04 18:01:30 -0500

    Fix resizing of images to match standard capture size

--- src/capture.py
+++ src/capture.py
@@ -61,7 +61,7 @@ def crop_to_playfield(img):
     @return: Image of the playfield.
     """
 
-    cv2.resize(img, (256, 224))
+    img = cv2.resize(img, (256, 224))
 
     x1, y1 = (88, 23) # Top Left
     x2, y2 = (x1  + 16 * 6, y1 + 16 * 12) # Bottom Right
@@ -76,7 +76,7 @@ def crop_to_moves(img):
     @return: Image of the number of moves.
     """
 
-    cv2.resize(img, (256, 224))
+    img = cv2.resize(img, (256, 224))
 
     x1, y1 = (195, 59) # Top Left
     x2, y2 = (x1 + 42, y1 + 48)
