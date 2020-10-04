c1ea0da1dce1c0461ab9a976a97cd648750ccdb0
jnorth314 <56890989+jnorth314@users.noreply.github.com>
2020-10-04 17:49:47 -0500

    Implement ability to capture window regions for game feed

--- src/capture.py
+++ src/capture.py
@@ -1,4 +1,57 @@
 import cv2
+import numpy
+import win32ui
+import win32gui
+import win32con
+
+def capture_window(hwnd, rect):
+    """
+    Uses the windows API to capture a region from a window.
+
+    @param hwnd: Handle to the window.
+    @param rect: Bounding box of the region
+    @return: Image of the window's region
+    """
+
+    width = rect[2] - rect[0]
+    height = rect[3]- rect[1]
+
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(dcObj, width, height)
+    cDC.SelectObject(bmp)
+    cDC.BitBlt((0, 0), (width, height), dcObj, (rect[0], rect[1]), win32con.SRCCOPY)
+
+    img = bmp.GetBitmapBits(True)
+    img = numpy.frombuffer(img, dtype='uint8')
+    img.shape = (height, width, 4)
+
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return img
+
+def get_window_rect(hwnd):
+    """
+    Get the rect of the window relative to client without borders.
+
+    @param hwnd: Handle to the window.
+    @return: Bounding box of the window's region.
+    """
+
+    rect = win32gui.GetWindowRect(hwnd)
+
+    # Left = 8 pixels of border
+    # Top = 30 pixels of border
+    # Right = 8 pixels of border
+    # Bottom = 8 pixels of border
+    rect = (8, 30, rect[2] - rect[0] - 8, rect[3] - rect[1] - 8)
+
+    return rect
 
 def crop_to_playfield(img):
     """
